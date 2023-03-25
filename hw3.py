a33edf465b8306adc9d37661a38bcd028881307b
sudulge <hhs0888@naver.com>
2023-03-25 17:33:55 +0900

    hw3 수정

--- hw3.py
+++ hw3.py
@@ -6,5 +6,5 @@ rate = int(input("할인율은? "))
 A_discounted_price = int(input("A 상품의 할인된 가격은? "))
 B_discounted_price = int(input("B 상품의 할인된 가격은? "))
 
-print("A 상품의 정가는", get_fixed_price(20, A_discounted_price), "원")
-print("B 상품의 정가는", get_fixed_price(20, B_discounted_price), "원")
+print("A 상품의 정가는", get_fixed_price(rate, A_discounted_price), "원")
+print("B 상품의 정가는", get_fixed_price(rate, B_discounted_price), "원")
