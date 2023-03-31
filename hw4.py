a631b8807d92f1c0f17264fa0f9e0007df191bb5
홍혁수 <57317487+sudulge@users.noreply.github.com>
2023-03-31 15:34:06 +0900

    hw4 제출

---
+++ hw4.py
@@ -0,0 +1,17 @@
+def rep_char(c, n):
+    print(c * n)
+
+def draw_line_string(string):
+    msg1 = f"Hello {string},"
+    msg2 = "Welcome to Seoul."
+    
+    nstr = len(msg1) if (len(msg1) > len(msg2)) else len(msg2)
+
+    rep_char('-', nstr)
+    print(msg1)
+    print(msg2)
+    rep_char('-', nstr)
+
+name = input("Input his/her name: ")
+
+draw_line_string(name)
